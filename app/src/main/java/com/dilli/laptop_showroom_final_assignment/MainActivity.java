7f1bfb8968dd05cef8b7ec3601215278cc08d327
dell <bhandaridilli1@gmail.com>
2019-12-14 16:26:53 +0545

    create the new project laptop showroom final assignment.

---
+++ app/src/main/java/com/dilli/laptop_showroom_final_assignment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dilli.laptop_showroom_final_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
